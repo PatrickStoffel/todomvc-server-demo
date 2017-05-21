c81074bc84441139e8b559278929c6ae34a97e04
Patrick Stoffel <patrick_stoffel@hotmail.com>
2017-05-21 18:31:35 +0200

    Übung API implementieren

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 'use strict'
 
 import express from 'express'
+import fs from 'fs'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
 let app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('Hello World!!')
+app.get('/items', function (req, res) {
+  let content = fs.readFileSync('data.json', 'utf8')
+  res.json(content)
+})
+
+app.post('/items', function (req, res) {
+    let data = req.body
+
+    let content = fs.readFileSync('data.json', 'utf8')
+    let json = JSON.parse(content)
+    json.push(data)
+
+    fs.writeFileSync('data.json', JSON.stringify(json), 'utf8')
+
+    res.json()
 })
 
 app.listen(3100)
